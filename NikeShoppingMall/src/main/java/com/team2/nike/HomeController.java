1509fff594df6517ed1d668d738c4d76bee6ba19
gnb577 <chlwodlwkd@naver.com>
2022-03-12 20:38:27 +0900

    Make Spring Project

---
+++ NikeShoppingMall/src/main/java/com/team2/nike/HomeController.java
@@ -0,0 +1,28 @@
+package com.team2.nike;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	//HttpServlet만 썼다가 오류 났음 HttpServletRequest이다.
+	@RequestMapping("/home.do")
+	public String home(HttpServletRequest request) {
+		return "home";
+	}
+}
