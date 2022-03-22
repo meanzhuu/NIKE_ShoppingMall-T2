d51bb21336c3c3e04a42308db162f1a8167131ff
meanzhuu <minju09721@gmail.com>
2022-03-22 16:31:10 +0900

    order mapper

--- NikeShoppingMall/src/main/java/com/team2/nike/mypage/controller/MypageController.java
+++ NikeShoppingMall/src/main/java/com/team2/nike/mypage/controller/MypageController.java
@@ -14,10 +14,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.team2.nike.product.dto.ProductDto;
-import com.team2.nike.product.service.ProductService;
+import com.team2.nike.orders.service.OrdersService;
 
 @Controller //수정 전 그냥 파일만들어둠
 public class MypageController {
+	
+	@Autowired
+	private OrdersService service;
+	@RequestMapping("/mypage/orders")
+	public ModelAndView list(ModelAndView mView) {
+		service.getListOrders(mView);
+		mView.setViewName("mypage/orders");
+		return mView;
+	}
 	
 }
