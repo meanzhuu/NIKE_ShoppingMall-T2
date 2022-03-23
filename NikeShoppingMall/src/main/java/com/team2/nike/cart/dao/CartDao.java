4548a21753163d782427031758a3c172025ff140
gnb577 <chlwodlwkd@naver.com>
2022-03-23 18:56:47 +0900

    결제 완료 후 장바구니 비우기

--- NikeShoppingMall/src/main/java/com/team2/nike/cart/dao/CartDao.java
+++ NikeShoppingMall/src/main/java/com/team2/nike/cart/dao/CartDao.java
@@ -8,5 +8,6 @@ public interface CartDao {
 	public List<CartDto>getList(String users_id); //장바구니의 목록
 	public void insert(CartDto dto); //장바구니에 추가하기
 	public void delete(int cart_id); //장바구니 상품 삭제
+	public void deleteAll(String users_id);
 	public CartDto getData(int cart_id); //
 }
